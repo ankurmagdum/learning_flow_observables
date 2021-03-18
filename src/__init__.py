4481df9ade47481698dfa2c159eb0ccbc9f0b39f
Ankur Magdum <theankurmagdum10@gmail.com>
2021-03-18 10:41:38 +0100

    updated __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from .hyperparameter_config import hyperparameterConfiguration
+from .hyperparameter_configuration import hyperparameterConfiguration
 from .seperate_test_data import seperate_test_data
 from .predict import predict
-from .error_stats import compute_error_statistics
+from .error_statistics import compute_error_statistics
